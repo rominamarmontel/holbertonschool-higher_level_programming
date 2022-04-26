8141027cace855236da5f4b283aa68739980573e
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-04-26 23:32:29 +0200

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    str = str[:n] + str[n+1:]
-    return str
+    copystr = str[:n] + str[n+1:]
+    if n >=0:
+        return copystr
+    else:
+        return str
