e1c02b99025b44d225eb007229001ba5b53a4579
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-04-26 16:43:30 +0200

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for letter in range(122, 96, -1):
+    if letter % 2 == 0:
+        letter = chr(letter)
+        print("{}".format(letter), end="")
+    else:
+        letter = chr(letter - 32)
+        print("{}".format(letter), end="")
