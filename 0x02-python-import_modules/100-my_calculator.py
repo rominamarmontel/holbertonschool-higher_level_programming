873c4e746af3138a295093d65390301b8c70e9c8
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-04-28 16:40:12 +0200

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
         if argv[2] == "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
         if argv[2] == "-":
-            print("{} + {} = {}".format(a, b, sub(a, b)))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         if argv[2] == "*":
-            print("{} + {} = {}".format(a, b, mul(a, b)))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         if argv[2] == "/":
-            print("{} + {} = {}".format(a, b, div(a, b)))
+            print("{} / {} = {}".format(a, b, div(a, b)))
