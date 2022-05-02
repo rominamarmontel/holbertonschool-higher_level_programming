bd1d576fb2aec864738a3c24a0cb2df11add4ac9
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-02 17:53:35 +0200

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in range(0, len(sentence)):
-        if i > 0:
-            return(i, sentence[0])
-        else:
-            return(0, None)
-        
+    count = len(sentence)
+    if count <= 0:
+        return (0, None)
+    else:
+        return (count, sentence[0])
