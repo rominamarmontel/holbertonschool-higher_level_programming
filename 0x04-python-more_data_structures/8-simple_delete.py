ff66739b240a1bd09d4be48d66b3610bb3f5c901
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-04 15:26:13 +0200

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for element in a_dictionary:
-        if element == key:
-            del a_dictionary[element]
-            return a_dictionary
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
