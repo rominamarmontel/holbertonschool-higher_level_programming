1fd58c60ba97814c87f2a2bacc812dc7a495a082
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-04 17:00:28 +0200

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    num = 0
-    pre = 0
     dico = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for i in range(len(roman_string) - 1, -1, -1):
-        c = roman_string[i]
-        if c not in dico or c is None:
+    num = 0
+    for char in roman_string:
+        if char not in dico or char is None:
             return 0
-        n = dico[c]
-        if n >= pre:
-            num += n
-            pre = n
-        else:
-            num -= n
+        num += dico[char]     
     return num
