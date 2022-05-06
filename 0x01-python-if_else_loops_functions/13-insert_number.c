4e0f229e81ecfdd4fe6aabcdcc64d026cfafc144
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-06 14:41:47 +0200

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,32 +9,31 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new = malloc(sizeof(listint_t));
-	listint_t *prev;
-	listint_t *temp = *head;
-
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-	while (temp != NULL)
-	{
-		if (temp->n > number)
-			break;
-		prev = temp;
-		temp = temp->next;
-	}
-	if (*head == NULL)
-	{
-		new->next = NULL;
-		*head = new;
-	}
-	else
-	{
-		new->next = temp;
-		if (temp == *head)
-			*head = new;
-		else
-			prev->next = new;
-	}
-	return (new);
+    listint_t *new = malloc(sizeof(listint_t));
+    listint_t *prev;
+    listint_t *temp = *head;
+    if (new == NULL)
+        return (NULL);
+    new->n = number;
+    while (temp != NULL)
+    {/*insert ichi wo kimeru*/
+        if (temp->n > number)/*insert ichi ga mitsukaru*/
+            break;
+        prev = temp;/*temp wo mae no node toshite oboeru*/
+        temp = temp->next;/*tsugi no node e susumu*/
+    }
+    if (*head == NULL)/*moshimo sento ga NULL no baai*/
+    {
+        new->next = NULL;
+        *head = new;/*list no sento wo new ni suru*/
+    }
+    else
+    {
+        new->next = temp;/*tuika ichi ga owari no baai wa temp*/
+        if (temp == *head)/*tsuika ichiga sento no baai*/
+            *head = new;/*sento ni new wo tuika*/
+        else
+            prev->next = new;
+    }
+    return (new);
 }
