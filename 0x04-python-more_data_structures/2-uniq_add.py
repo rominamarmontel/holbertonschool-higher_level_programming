38ca3ba8766a79c349c2b0feda190fc20cb68960
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-22 14:36:17 +0200

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = []
+    new_list = set(my_list)
     sum = 0
-    for i in my_list:
-        if i not in new_list:
-            new_list.append(i)
-            sum += i
+    for i in new_list:
+        sum += i
     return sum
