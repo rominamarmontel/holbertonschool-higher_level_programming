d38860c0f5e7867cf8b22f70738b8962c24beec4
rominamarmontel <romi.varnier@gmail.com>
2022-05-23 18:51:20 +0200

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,40 +6,16 @@ class Rectangle:
     """ Definition of a Class Rectangle
 
     """
-    def __init__(self, width=0, height=0):
-        if type(width) != int:
-            raise TypeError("width must be integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
-        if type(height) != int:
-            raise TypeError("height must be integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
-
-        @property
-        def width(self):
-            return self.__width
-
-        @width.setter
-        def width(self, value):
-            """initializes the Rectangle
-            Args:
-                width : integer
-            """
-            if type(value) != int:
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
-
+    def __init__(self, width=0, height=0):       
+        self.height = height
+        self.width = width
+        
         @property
         def height(self):
             return self.__height
+            """ The method : height
+            Return : the height of rectangle
+            """
 
         @height.setter
         def height(self, value):
@@ -53,3 +29,23 @@ class Rectangle:
                 raise ValueError("height must be >= 0")
             else:
                 self.__height = value
+
+        @property
+        def width(self):
+            return self.__width
+            """ The method : width
+            Return : the width of rectangle
+            """
+
+        @width.setter
+        def width(self, value):
+            """initializes the Rectangle
+            Args:
+                width : integer
+            """
+            if type(value) != int:
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
