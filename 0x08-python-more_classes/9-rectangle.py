f2b022692f81ec089e69399bf95cc9440e425806
rominamarmontel <romi.varnier@gmail.com>
2022-05-23 19:11:18 +0200

    modifier error

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,19 +10,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        if type(width) != int:
-            raise TypeError("width must be integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
-        if type(height) != int:
-            raise TypeError("height must be integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
-
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
