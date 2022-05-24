96331a8141d93db8e83d73d0c8fffc928c41d644
rominamarmontel <romi.varnier@gmail.com>
2022-05-25 00:21:26 +0200

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,7 +12,6 @@ def add_integer(a, b=98):
     @b: integer
     Return: the add of a and b, an exact integer
     """
-
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
