8b62dfded4c1e948251486f81c5cc2e8633d88b7
Romi <romi@MacBook-Pro-de-Romi.local>
2022-05-29 12:52:00 +0200

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,31 +9,23 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new = malloc(sizeof(listint_t));
-    listint_t *prev;
-    listint_t *temp = *head;
-    if (new == NULL)
-        return (NULL);
-    new->n = number;
-    while (temp != NULL)
-    {/*insert ichi wo kimeru*/
-        if (temp->n > number)/*insert ichi ga mitsukaru*/
-            break;
-        prev = temp;/*temp wo mae no node toshite oboeru*/
-        temp = temp->next;/*tsugi no node e susumu*/
-    }
-    if (*head == NULL)/*moshimo sento ga NULL no baai*/
-    {
-        new->next = NULL;
-        *head = new;/*list no sento wo new ni suru*/
-    }
-    else
-    {
-        new->next = temp;/*tuika ichi ga owari no baai wa temp*/
-        if (temp == *head)/*tsuika ichiga sento no baai*/
-            *head = new;/*sento ni new wo tuika*/
-        else
-            prev->next = new;
-    }
-    return (new);
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	if (*head == NULL || (*head)->n > number)
+	{
+		*head = new;
+		new->next = temp;
+	}
+	else
+	{
+		while (temp->next != NULL && number > temp->next->n)
+			temp = temp->next;
+		new->next = temp->next;
+		temp->next = new;
+	}
+	return (new);
 }
