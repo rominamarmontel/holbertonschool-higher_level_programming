ad82b3afb655ce3667d59c45d8cb44ca90fead15
Romi <romi@MacBook-Pro-de-Romi.local>
2022-05-30 18:41:30 +0200

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,40 +4,11 @@ Define Square class
 """
 
 
-class BaseGeometry:
-    """ class BaseGeometry """
-    def area(self):
-        """ Area : Define Instance Method """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Value : Define Instance Method """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        elif 0 >= value:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """ class Rectangle inherits from BaseGeometory"""
-    def __init__(self, width, height):
-        """ Initialize rectangle constructor"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """ Area : Define Instance Method """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ Str : Rectangle description """
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ class Square inherits from Rectangle"""
+    """ class Square inherits from Rectangle """
     def __init__(self, size):
         """ Initialize square constructor """
         self.integer_validator("size", size)
