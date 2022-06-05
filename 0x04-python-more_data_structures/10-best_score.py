83d4a82a5e8007fd67a611aa94343ffeea5d70ac
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-05 20:39:09 +0200

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        max_k = max(a_dictionary, key=a_dictionary.get)
-        return max_k
+        max = 0
+        for key in a_dictionary:
+            if a_dictionary[key] > max:
+                max = a_dictionary[key]
+                best_score = key
+        return best_score
     return None
