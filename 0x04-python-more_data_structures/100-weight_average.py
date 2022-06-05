fceb82415fbe24fb71944ae4a402cd2348203c45
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-05 21:51:18 +0200

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == []:
-        return 0
-    sum1 = 0
-    sum2 = 0
-    for i in range(len(my_list)):
-        sum1 += my_list[i][0] * my_list[i][1]
-        sum2 += my_list[i][1]
-    return sum1 / sum2
+    if my_list:
+        sum1 = 0
+        sum2 = 0
+        for i in range(len(my_list)):
+            sum1 += my_list[i][0] * my_list[i][1]
+            sum2 += my_list[i][1]
+        return sum1 / sum2
+    return None
