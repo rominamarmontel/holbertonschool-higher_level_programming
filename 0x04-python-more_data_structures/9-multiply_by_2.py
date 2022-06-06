158b2d2696dac1efb9e590f3bb38c20e9ed811fa
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-06 18:08:50 +0200

    9-multiply_by_2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_a_dictionary = a_dictionary.copy()
-    for value in a_dictionary:
-        new_a_dictionary[value] = a_dictionary[value] * 2
-    return new_a_dictionary
+    new_dictionary = a_dictionary.copy()
+    for key in new_dictionary:
+        new_dictionary[key] = new_dictionary[key] * 2
+    return new_dictionary
