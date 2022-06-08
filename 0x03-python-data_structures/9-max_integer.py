9864f24220d0febb84cecf793970aef1d4de139d
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-08 21:30:23 +0200

    9-max_integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    while my_list:
-        max = my_list[0]
-        for i in my_list:
-            if i > max:
-                max = i
-        return max
-    return None
+	if my_list == []:
+		return None
+	max = 0
+	for i in my_list:
+		if i > max:
+			max = i
+	return max
+	
