e9bae7438729d4287f34abbec2d93b36b707f859
rominamarmontel <romi.varnier@gmail.com>
2022-06-25 19:35:21 +0200

    update

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
             width : a private attribute
             height : a private attribute
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
