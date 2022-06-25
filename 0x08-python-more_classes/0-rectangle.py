e3d62b4a9e4183b8ca830c43b23d209596ae66e0
rominamarmontel <romi.varnier@gmail.com>
2022-06-25 19:38:00 +0200

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" Module Rectangle """
+"""
+    Module Rectangle
+"""
 
 
 class Rectangle:
-    """ Definition of a Class Rectangle
-
+    """
+    Definition of a Class Rectangle
     """
     pass
