00828494a6934b29a7fe256394316966d184ca3e
rominamarmontel <romi.varnier@gmail.com>
2022-06-25 21:10:20 +0200

    update

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,25 +4,7 @@ Define class BaseGeometry
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """ class Rectangle inherits from BaseGeometory"""
-    def __init__(self, width, height):
-        """ Initialize rectangle constructor"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """ Area : Define Instance Method """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ Str : Rectangle description """
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
