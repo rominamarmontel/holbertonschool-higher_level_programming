00840d0e9f7658e011bbd65c7b960a6f680071d3
rominamarmontel <romi.varnier@gmail.com>
2022-06-25 20:11:22 +0200

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,7 +9,6 @@ class Rectangle:
     Definition of a Class Rectangle
     """
     number_of_instances = 0
-    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """
@@ -20,6 +19,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
