de3dddc333f56a9e3f12a46135dd387519f2cc7e
rominamarmontel <romi.varnier@gmail.com>
2022-07-06 13:48:24 +0200

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
--- script that creates the table force_name
-CREATE TABLE IF NOT EXISTS force_name (
-	id INT,
-	name VARCHAR(256) NOT NULL
-);
+-- script that creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
