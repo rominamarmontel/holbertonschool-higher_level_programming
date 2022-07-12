ac8d289cf22cd01c20e9ea269a3f0d1b6439c2d4
rominamarmontel <romi.varnier@gmail.com>
2022-07-12 17:04:53 +0200

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for number in range(0, 90):
-	digit1 = number / 10
-	digit2 = number % 10
-	if number == 89:
-		print(89)
-	elif digit1 < digit2:
-		print("{:02d}, ".format(number), end="")
+    digit1 = number / 10
+    digit2 = number % 10
+    if number == 89:
+        print(89)
+    elif digit1 < digit2:
+        print("{:02d}, ".format(number), end="")
