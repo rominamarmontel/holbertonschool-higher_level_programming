8bbd3a32a9f832237764a53e62b1926f0be11c64
rominamarmontel <romi.varnier@gmail.com>
2022-07-12 21:21:07 +0200

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        best = list(a_dictionary.keys())[0]
-        for key in a_dictionary:
-            if a_dictionary[key] > a_dictionary[best]:
-                best = key
+        best = list(a_dictionary)[0]
+        for i in a_dictionary:
+            if a_dictionary[i] > a_dictionary[best]:
+                best = i
         return best
     return None
