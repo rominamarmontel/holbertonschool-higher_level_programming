69916882f2081cc368795adf4412a185c87c16f7
rominamarmontel <romi.varnier@gmail.com>
2022-07-29 11:37:33 +0200

    100-not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- list all genres not linked to the show Dexter 
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name <> 'Dexter'
+GROUP BY tv_genres.name
+ORDER BY tv_genres.name;
