59303004b4634505506aeaf4756e39cf0ab9cb3b
rominamarmontel <romi.varnier@gmail.com>
2022-07-29 11:40:31 +0200

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,6 +5,14 @@ JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_genres.name <> 'Dexter'
+WHERE tv_genres.name IS NOT IN (
+    SELECT tv_genres.name
+    FROM tv_genres
+    JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows
+    ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title='Dexter'
+)
 GROUP BY tv_genres.name
 ORDER BY tv_genres.name;
