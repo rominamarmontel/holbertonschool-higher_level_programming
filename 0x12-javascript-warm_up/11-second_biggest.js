84bd26af700c3178a35abad211b9e99a285b001c
rominamarmontel <romi.varnier@gmail.com>
2022-08-01 15:00:15 +0200

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,8 +7,8 @@ if (argv.length < 4) {
   for (let i = 2; i < argv.length; i++) {
     arr[i - 2] = parseInt(argv[i]);
   }
-  const max = Math.max.apply(null, arr); // get the max of the array
-  arr.splice(arr.indexOf(max), 1); // remove max from the array
-  const secondMax = Math.max.apply(null, arr)
-  console.log(secondMax); // get the 2nd max
+  const max = Math.max.apply(null, arr);
+  arr.splice(arr.indexOf(max), 1);
+  const secondMax = Math.max.apply(null, arr);
+  console.log(secondMax);
 }
