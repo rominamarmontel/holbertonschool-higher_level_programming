16cca869d9a9384580190901664bb04c48890370
rominamarmontel <romi.varnier@gmail.com>
2022-08-01 13:07:51 +0200

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const argv = process.argv;
 if (argv.length < 3) {
   console.log('No argument');
-} else if (argv.length >= 3) {
+} else if (argv.length == 3) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
