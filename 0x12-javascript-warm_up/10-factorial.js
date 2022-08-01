63d9c70100f49a6437b95d1544be112b308c7802
rominamarmontel <romi.varnier@gmail.com>
2022-08-01 14:49:31 +0200

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -9,7 +9,7 @@ if (isNaN(argv[2])) {
 function factorial (num) {
   if (num === 0) {
     return (1);
-} else {
-  return (num * factorial(num - 1));
-}
+  } else {
+    return (num * factorial(num - 1));
+  }
 }
