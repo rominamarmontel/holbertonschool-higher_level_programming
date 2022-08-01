9c73bb7db786b72c098f16eff64f1f3d79523786
rominamarmontel <romi.varnier@gmail.com>
2022-08-01 14:00:17 +0200

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-const arrayLength = array.length;
-for (i = 0; i < arrayLength; i++) {
-  console.log(array[i]);
+for (i = 0; i < array.length; i++) {
+	console.log(array[i]);
 }
