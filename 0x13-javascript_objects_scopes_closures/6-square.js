f189bad1766e2fab404174266f7e82648ba256ca
rominamarmontel <romi.varnier@gmail.com>
2022-08-02 12:12:19 +0200

    update js data

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      if (c === 'C') {
+        console.log('C'.repeat(this.size));
+      } else {
+        console.log('X'.repeat(this.size));
+      }
+    }
+  }
+};
