7b26dcfcfaaf419c51d91f5c53e1ccf7ad64932e
romi <romi.varnier@gmail.com>
2022-08-06 13:52:45 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,16 +6,18 @@ import sys
 
 if __name__ == "__main__":
     arg = sys.argv
-    connection = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
-                                 passwd=arg[2], db=arg[3], charset="utf8")
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=arg[1],
+        passwd=arg[2],
+        db=arg[3],
+        charset="utf8")
     cursor = connection.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states
-    ON cities.state_id = states.id
-    ORDER BY cities.id ASC")
-    query_rows = cursor.fetchall()
-    for row in query_rows:
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
     cursor.close()
     connection.close()
