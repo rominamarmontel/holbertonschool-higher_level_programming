2b4900ac804db16fd3dc25a34f02b7b273628bb9
romi <romi.varnier@gmail.com>
2022-08-06 14:06:39 -0700

    ./5-filter_cities.py root root hbtn_0e_4_usa Hawaii

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,22 +7,20 @@ import sys
 
 if __name__ == "__main__":
     arg = sys.argv
-    connection = MYSQLdb.connect(host="localhost",
-                                 port=3306,
-                                 user=arg[1],
-                                 password=arg[2],
-                                 db=arg[3],
-                                 charset="utf8")
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=arg[1],
+        password=arg[2],
+        db=arg[3],
+        charset="utf8")
     cursor = connection.cursor()
-    cursor.execute("SELECT cities.name
-    FROM cities
-    JOIN states
-    ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC", (arg[4],))
-    query_rows = cursor.fetchall()
+    cursor.execute("SELECT cities.name FROM cities JOIN states \
+    ON cities.state_id = states.id WHERE states.name = %s \
+    ORDER BY cities.id", (arg[4],))
+    rows = cursor.fetchall()
     array = []
-    for row in query_rows:
+    for row in rows:
         array.append(row[0])
     print(", ".join(array))
     cursor.close()
