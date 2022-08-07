2c338c33033f12ea55bfeb32fec6187f7d3970f6
romi <romi.varnier@gmail.com>
2022-08-07 07:35:18 -0700

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Write a python file that contains the class definition
+of a State and an instance"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ The Class States"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name =  Column(String(128), nullable=False)
