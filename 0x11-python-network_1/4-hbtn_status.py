ba43e61239a1fd993710f87fd1b0c89cc02b7d7f
rominamarmontel <romi.varnier@gmail.com>
2022-08-19 12:58:05 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Fetches https://intranet.hbtn.io/status """
+import requests
+
+if __name__ == "__main__":
+    response = requests.get('https://intranet.hbtn.io/status')
+    html = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
