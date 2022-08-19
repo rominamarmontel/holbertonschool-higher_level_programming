42caf6bd4a1d614f66e8c67157d1557581bea2f1
rominamarmontel <romi.varnier@gmail.com>
2022-08-19 13:00:27 +0000

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" Fetches https://intranet.hbtn.io/status """
+import requests
+import sys
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers.get('X-Request-Id'))
