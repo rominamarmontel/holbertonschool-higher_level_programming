a62ea3d0438b0c72e4c04cecdf2e449391db1f43
rominamarmontel <romi.varnier@gmail.com>
2022-08-19 12:22:06 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Write a Python script that fetches https://intranet.hbtn.io/status"""
+"""fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
+
+if __name__ == "__main__":
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
