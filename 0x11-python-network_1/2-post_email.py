8c00687c2baa868bbe5863b33278b671c376a437
rominamarmontel <romi.varnier@gmail.com>
2022-08-30 15:06:34 +0000

    4-header.sh

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""displays the body of the response (decoded in utf-8)"""
+import urllib.parse
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    data = parse.urlencode(email)
+    data = data.encode('ascii')
+
+    with urllib.request.urlopen(sys.argv[1], data) as response:
+        print(response.read().decode('utf-8'))
