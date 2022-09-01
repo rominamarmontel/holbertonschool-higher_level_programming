f2e0e893a3ec1c255e420f0ee6bb6ede31c13307
rominamarmontel <romi.varnier@gmail.com>
2022-09-01 09:01:28 +0000

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""sends a request to the URL and displays the body of the response"""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import sys
+
+if __name__ == "__main__":
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(reponese.read().decode('utf-8'))
+    except HTTPError as error:
+        print('Error code: {}'.format(error.code))
