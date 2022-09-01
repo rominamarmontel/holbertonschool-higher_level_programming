7ab42446e9c1790628c0a80a27ba876881c3d467
rominamarmontel <romi.varnier@gmail.com>
2022-09-01 09:04:00 +0000

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
 
     try:
-        with request.urlopen(sys.argv[1]) as response:
+        with request.urlopen(argv[1]) as response:
             print(reponese.read().decode('utf-8'))
     except HTTPError as error:
         print('Error code: {}'.format(error.code))
