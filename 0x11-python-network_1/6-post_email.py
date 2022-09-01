f68fee9b126e47a3b48fd0abb3608991ad3aa868
rominamarmontel <romi.varnier@gmail.com>
2022-09-01 09:11:52 +0000

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" Fetches https://intranet.hbtn.io/status """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    email = {'email': argv[2]}
+    request = requests.post(argv[1], data=email)
+    print(request.text)
