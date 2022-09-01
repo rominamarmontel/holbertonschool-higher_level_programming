bd4853aa38a1fd4ec87519992150299b231e12a2
rominamarmontel <romi.varnier@gmail.com>
2022-09-01 09:15:00 +0000

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Fetches https://intranet.hbtn.io/status """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    request = requests.get(argv[1])
+
+    if request.status_code < 400:
+        print(request.text)
+    else:
+        print('Error code: {}'.format(request.status_code))
