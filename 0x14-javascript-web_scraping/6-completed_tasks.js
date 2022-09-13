8871c14445a645420e0b4d7085ec059d89e8124c
rominamarmontel <romi.varnier@gmail.com>
2022-09-13 10:23:35 +0000

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+//Write a script that computes the number of tasks completed by user id
+const axios = require('axios').default;
+axios.get(process.argv[2])
+    .then(function (response) {
+        const dico = {};
+        for (let i = 0; i < list.length; i++) {
+            if (response.data[i].completed === true) {
+                if (!(response.data[i].userId in dico)) {
+                    dico[response.data[i].userId] = 1;
+                    } else {
+                        dico[response.data[i].userId] += 1;
+                        }
+                    }
+                }
+                console.log(dico);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
