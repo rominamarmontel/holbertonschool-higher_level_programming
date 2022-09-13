2ed33515ed0dc73829dfd0efa2a481d89c92f34f
rominamarmontel <romi.varnier@gmail.com>
2022-09-13 05:10:31 -0700

    error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (error, data) => {
+const path = process.argv[2]
+fs.readFile(path, 'utf8', (error, data) => {
     if(data) {
         console.log(data);
     } else {
