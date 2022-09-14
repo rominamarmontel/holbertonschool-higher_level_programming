49fa03f2eeeb7d153490a728831ef1173264bfa3
rominamarmontel <romi.varnier@gmail.com>
2022-09-14 11:35:45 -0700

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const argv = process.argv;
-const value = parseInt(argv[2], 10);
-if (isNaN(value)) {
+const arg = parseInt(process.argv[2]);
+if (isNaN(arg)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < argv[2]; i++) {
-    console.log('X'.repeat(argv[2]));
+  for (let i = 0; i < arg; i++) {
+    console.log('X'.repeat(arg));
   }
 }
