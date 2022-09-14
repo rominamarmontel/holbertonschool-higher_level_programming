d978a407d7f6fda74f26992e3eef69e1543d163f
rominamarmontel <romi.varnier@gmail.com>
2022-09-14 11:10:35 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const args = process.argv;
-const value = parseInt(args[2], 10);
-if (isNaN(value)) {
+const arg = parseInt(process.argv[2]);
+if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + value);
+  console.log('My number: ' + arg);
 }
