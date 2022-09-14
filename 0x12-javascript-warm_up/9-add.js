fe4c9a1cfd3cdd860f6b973c2f6e1997a1038766
rominamarmontel <romi.varnier@gmail.com>
2022-09-14 11:57:11 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const argv = process.argv;
-console.log(add(argv[2], argv[3]));
-
 function add (a, b) {
-  return (parseInt(a) + parseInt(b));
+  return (a + b);
 }
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
